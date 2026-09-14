da18440876064990dc1dc5fa00d899b1cdd65cc5
Andy De Leon <66084673+andy-jdl@users.noreply.github.com>
2026-09-14 16:49:54 -0700

    permalink

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add('src/11ty/_site/**');
   return {
-    pathPrefix: "/lacmta-express/",
     dir: {
       input: 'src/11ty',
       output: 'docs',
